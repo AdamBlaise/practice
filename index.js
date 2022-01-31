03436ce9c5da64fc50ef35846503f26f54794091
AdamBlaise <59825894+AdamBlaise@users.noreply.github.com>
2022-01-31 19:48:48 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,35 @@
+let incrementEl = document.getElementById("increment")
+let countEl = document.getElementById("start-btn")
+let saveEl = document.getElementById("save-el")
+let sumEl = document.getElementById("sum-el")
+let arr = []
+let count = 0
+let sum = 0
+let counting = 0
+
+function people() {
+    count++
+    incrementEl.textContent = count
+}
+
+function save(){
+    counting = `${count} - `
+    saveEl.textContent += counting
+    //countEl.innerText = 0
+    count = 0
+}
+
+function total(){
+    let num = save()
+    arr = [num]
+    sum += arr
+    show()
+}
+
+function show(){
+    sumEl.textContent = "Total Entries: "
+    for( let i = 0; i < cards.length; i++){
+        sumEl.textContent += arr[i] + " "
+    }
+    sumEl.textContent = "Sum: " + sum;
+}
